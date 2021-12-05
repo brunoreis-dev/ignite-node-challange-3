fb786ecbcaf137198d3cd12a9d62bbb03f9443d9
Bruno Reis <b.macedoreis@icloud.com>
2021-12-05 00:39:32 -0300

    initial commit

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,40 @@
+import { getRepository, Repository } from "typeorm";
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOneOrFail(user_id, {
+      relations: ["games"],
+    });
+
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query(`
+      SELECT  *
+      FROM users
+      ORDER BY first_name ASC;
+    `);
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(`
+      SELECT * FROM users WHERE lower(first_name)=lower('${first_name}') AND lower(last_name)=lower('${last_name}');
+    `);
+  }
+}
